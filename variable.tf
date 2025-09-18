cd884b3bd2a9fa6a3e0679d9d235d13a41d62148
Mainul Islam <mainulislam6168@gmail.com>
2025-09-18 12:20:27 +0600

    catalogue secrets are kept in vault

--- variable.tf
+++ variable.tf
@@ -17,8 +17,8 @@ variable "secrets" {
     cart = {
       sercet_mount = "roboshop-dev"
       kv = {
-        REDIS_HOST           = "redis-dev.maidevops.fun",
-        CATALOGUE_HOST       = "catalogue-dev.maidevops.fun",
+        REDIS_HOST           = "redis-dev.maidevops.fun"
+        CATALOGUE_HOST       = "catalogue-dev.maidevops.fun"
         CATALOGUE_PORT       = 8080
       }
     }
@@ -26,20 +26,20 @@ variable "secrets" {
     frontend = {
       sercet_mount = "roboshop-dev"
       kv = {
-        CATALOGUE_URL    = "http://catalogue-dev.maidevops.fun:8080/",
-        USER_URL         = "http://user-dev.maidevops.fun:8080/",
-        CART_URL         = "http://cart-dev.maidevops.fun:8080/",
-        SHIPPING_URL     = "http://shipping-dev.maidevops.fun:8080/",
-        PAYMENT_URL      = "http://payment-dev.maidevops.fun:8080/",
-        CATALOGUE_HOST   = "catalogue-dev.maidevops.fun",
-        CATALOGUE_PORT   = "8080",
-        USER_HOST        = "user-dev.maidevops.fun",
-        USER_PORT        = "8080",
-        CART_HOST        = "cart-dev.maidevops.fun",
-        CART_PORT        = "8080",
-        SHIPPING_HOST    = "shipping-dev.maidevops.fun",
-        SHIPPING_PORT    = "8080",
-        PAYMENT_HOST     = "payment-dev.maidevops.fun",
+        CATALOGUE_URL    = "http://catalogue-dev.maidevops.fun:8080/"
+        USER_URL         = "http://user-dev.maidevops.fun:8080/"
+        CART_URL         = "http://cart-dev.maidevops.fun:8080/"
+        SHIPPING_URL     = "http://shipping-dev.maidevops.fun:8080/"
+        PAYMENT_URL      = "http://payment-dev.maidevops.fun:8080/"
+        CATALOGUE_HOST   = "catalogue-dev.maidevops.fun"
+        CATALOGUE_PORT   = "8080"
+        USER_HOST        = "user-dev.maidevops.fun"
+        USER_PORT        = "8080"
+        CART_HOST        = "cart-dev.maidevops.fun"
+        CART_PORT        = "8080"
+        SHIPPING_HOST    = "shipping-dev.maidevops.fun"
+        SHIPPING_PORT    = "8080"
+        PAYMENT_HOST     = "payment-dev.maidevops.fun"
         PAYMENT_PORT     = "8080"
 
 
@@ -49,16 +49,21 @@ variable "secrets" {
     catalogue = {
       sercet_mount = "roboshop-dev"
       kv = {
-        MONGO           = "true",
+        MONGO           = "true"
         MONGO_URL       = "mongodb://mongodb-dev.maidevops.fun:27017/catalogue"
+
+        DB_TYPE         = "mongo"
+        APP_GIT_URL     = "https://github.com/roboshop-devops-project-v3/catalogue"
+        DB_HOST         = "mongodb-dev.maidevops.fun"
+        SCHEMA_FILE     = "db/master-data.js"
       }
     }
 
     user = {
       sercet_mount = "roboshop-dev"
       kv = {
-        MONGO           = "true",
-        REDIS_URL       = "redis://redis-dev.maidevops.fun:6379",
+        MONGO           = "true"
+        REDIS_URL       = "redis://redis-dev.maidevops.fun:6379"
         MONGO_URL       = "mongodb://mongodb-dev.maidevops.fun:27017/users"
       }
     }
@@ -73,7 +78,7 @@ variable "secrets" {
     rabbitmq = {
       sercet_mount = "roboshop-dev"
       kv = {
-        APP_USER           = "roboshop",
+        APP_USER           = "roboshop"
         APP_PASSWORD       = "roboshop123"
       }
     }
@@ -81,9 +86,9 @@ variable "secrets" {
     shipping = {
       sercet_mount = "roboshop-dev"
       kv = {
-        CART_ENDPOINT       = "cart-dev.maidevops.fun:8080",
-        DB_HOST             = "mysql-dev.maidevops.fun",
-        DB_USER             = "root",
+        CART_ENDPOINT       = "cart-dev.maidevops.fun:8080"
+        DB_HOST             = "mysql-dev.maidevops.fun"
+        DB_USER             = "root"
         DB_PASS             = "RoboShop@1"
       }
     }
@@ -91,12 +96,12 @@ variable "secrets" {
     payment = {
       sercet_mount = "roboshop-dev"
       kv = {
-        CART_HOST       = "cart-dev.maidevops.fun",
-        CART_PORT       = 8080,
-        USER_HOST       = "user-dev.maidevops.fun",
-        USER_PORT       = 8080,
-        AMQP_HOST       = "rabbitmq-dev.maidevops.fun",
-        AMQP_USER       = "roboshop",
+        CART_HOST       = "cart-dev.maidevops.fun"
+        CART_PORT       = 8080
+        USER_HOST       = "user-dev.maidevops.fun"
+        USER_PORT       = 8080
+        AMQP_HOST       = "rabbitmq-dev.maidevops.fun"
+        AMQP_USER       = "roboshop"
         AMQP_PASS       = "roboshop123"
       }
     }
@@ -104,7 +109,7 @@ variable "secrets" {
     ssh = {
       sercet_mount = "roboshop-infra"
       kv = {
-        username       = "ec2-user",
+        username       = "ec2-user"
         password        = "DevOps321"
       }
     }
