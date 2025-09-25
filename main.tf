d937468e751265d39c04bac13604ca5e01b930b9
Mainul Islam <mainulislam6168@gmail.com>
2025-09-25 16:35:21 +0600

    vault basic setup

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "vault_mount" "component" {
+  path        = "cart-dev"
+  type        = "kv-v2"
+  options = {
+    version = "2"
+    type    = "kv-v2"
+  }
+  description = "cart-dev"
+}
---
+++ terraform.tf
@@ -0,0 +1,12 @@
+provider "vault" {
+  address = "http://vault-internal.maidevops.fun:8200"
+  token = var.token
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-myshop"
+    key    = "roboshop-config-values/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
